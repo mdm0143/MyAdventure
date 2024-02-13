2d6467e1a3f3ffff2f3d145534013de8cf3201af
Micah Moore <micah.moore12@gmail.com>
2024-02-12 22:20:30 -0600

    Initial Commit

---
+++ MyAdventure/MyAdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAdventureApp.swift
+//  MyAdventure
+//
+//  Created by Micah Moore on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
